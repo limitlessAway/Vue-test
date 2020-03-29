659e685e63fc50ff0fb995100749d777b9f81c1b
limitlessAway <hristogk93@gmail.com>
2020-03-29 14:11:13 +0300

    Change CreateTheme to use vue class component

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,17 @@
     <h3>Documets</h3>
     <v-treeview activatable :items="items" :open-on-click="true"></v-treeview>
     <CreateTheme></CreateTheme>
-    <!-- <MyComponent></MyComponent> -->
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 import CreateTheme from "./components/CreateTheme";
-// import MyComponent from "./components/Test";
+
 export default {
   computed: mapGetters(["items"]),
   components: {
-    CreateTheme,
-    // MyComponent
+    CreateTheme
   }
 };
 </script>
--- src/components/CreateTheme.vue
+++ src/components/CreateTheme.vue
@@ -13,20 +13,14 @@
             <v-container>
               <v-row>
                 <v-col>
-                  <v-text-field
-                    v-model="themeTitle"
-                    label="Enter theme title"
-                    required
-                  ></v-text-field>
+                  <v-text-field v-model="themeTitle" label="Enter theme title" required></v-text-field>
                 </v-col>
               </v-row>
             </v-container>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="dialog = false"
-              >Close</v-btn
-            >
+            <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
             <v-btn color="blue darken-1" text @click="confirm">Save</v-btn>
           </v-card-actions>
         </v-card>
@@ -35,24 +29,45 @@
   </v-app>
 </template>
 
-<script>
-import { mapMutations } from "vuex";
-export default {
-  data: () => ({
-    dialog: false,
-    themeTitle: ""
-  }),
-  methods: {
-    // createTheme() {
-    // this.$emit("createTheme", [this.themeTitle]);
-    ...mapMutations(["createTheme"]),
-    confirm() {
-      this.createTheme({
-        name: this.themeTitle,
-        id: 99
-      });
-      (this.themeTitle = ""), (this.dialog = false);
-    }
+<script lang="ts">
+import { mapMutations, MutationMethod } from "vuex";
+import Component from "vue-class-component";
+import Vue from "vue";
+
+@Component({
+  methods: mapMutations(["createTheme"])
+})
+export default class CreateTheme extends Vue {
+  public dialog: boolean = false;
+  public themeTitle: string = "";
+  public createTheme!: MutationMethod;
+
+  public confirm(): void {
+    this.createTheme({
+      name: this.themeTitle,
+      id: 99
+    });
+    this.themeTitle = "";
+    this.dialog = false;
   }
-};
+}
+// import { mapMutations } from "vuex";
+// export default {
+//   data: () => ({
+//     dialog: false,
+//     themeTitle: ""
+//   }),
+//   methods: {
+//     // createTheme() {
+//     // this.$emit("createTheme", [this.themeTitle]);
+//     ...mapMutations(["createTheme"]),
+//     confirm() {
+//       this.createTheme({
+//         name: this.themeTitle,
+//         id: 99
+//       });
+//       (this.themeTitle = ""), (this.dialog = false);
+//     }
+//   }
+// };
 </script>
